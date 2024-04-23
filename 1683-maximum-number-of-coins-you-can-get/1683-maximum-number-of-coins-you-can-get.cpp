80b390be5f24dd76ae480fe07fcd9ccb801989f5
Manas Adhikari <152010406+Manasadhikari05@users.noreply.github.com>
2024-04-23 23:17:56 +0530

    Time: 89 ms (54.27%), Space: 56.7 MB (97.06%) - LeetHub

--- 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.cpp
+++ 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.cpp
@@ -4,7 +4,7 @@ public:
         sort(piles.begin(),piles.end());
         int n=piles.size();
         int count=0;
-        for(int i = n / 3; i < n; i += 2) {
+        for(int i = n/3; i<n; i=i+2) {
             count += piles[i];
         }
         return count;
