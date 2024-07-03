a67682c703a196c31d305e903a4af9352c97e5ab
Manas Adhikari <152010406+Manasadhikari05@users.noreply.github.com>
2024-07-03 16:35:19 +0530

    Time: 60 ms (57.92%), Space: 34.5 MB (23.21%) - LeetHub

---
+++ 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int n=items.size();
+        int count=0;
+            for(auto &it:items)
+            {
+                if(ruleKey=="type")
+                {
+                    if(it[0]==ruleValue)
+                    count++;
+                }
+                else if(ruleKey=="color")
+                {
+                    if(it[1]==ruleValue)
+                    count++;
+                }
+                else if(ruleKey=="name")
+                {
+                    if(it[2]==ruleValue)
+                    count++;
+                }
+            }
+        
+        return count;
+    }
+};
